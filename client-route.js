c6af9af3ef8e456d997a5b846fbfb9d6e1a59ae2
sandy1565 <40385371+sandy1565@users.noreply.github.com>
2019-02-20 18:53:45 +0530

    client

--- client-route.js
+++ client-route.js
@@ -3,6 +3,177 @@ var router = express.Router();
 var connection = require('./db.connection');
 const authJwt = require('./auth/verifyToken');
 
+
+
+router.get("/",[authJwt.verifyToken],function(req,res){
+    const queryStr = 'select * from client_master where admin_user_name=?';
+    connection.query(queryStr, [req.username], async function (err, rows) {
+        if(err){
+            res.status(401).send({
+                error:true,
+                data:[],
+                message:"No Client records Found"
+            });
+            return;
+        }
+        res.send({
+            data:rows,
+            message:'successfully fetched client records'
+        });
+    });
+});
+
+router.get("/:client_id",[authJwt.verifyToken],  function(req,res){
+    const queryStr = 'select * from client_master where client_id=? and admin_user_name=?';
+    connection.query(queryStr, [req.params.client_id,req.username], async function (err, rows) {
+        let record = rows[0];
+        if(err || !record){
+            res.status(401).send({
+                error:true,
+                message:"No Client Details Found"
+            });
+            return;
+        }
+        let profile_id = record.profile_id;
+        let document_id = record.document_id;
+        let profile_data = null;
+        let document_data = null;
+        if(profile_id){
+             profile_data = await getDocument(profile_id);
+             if(profile_data){
+                record.profile_pic = profile_data.file_data;
+             }
+        }
+        if(document_id){
+            document_data = await getDocument(document_id);
+            if(document_data){
+                record.document_file = document_data.file_data;
+            }
+        }
+        res.send({
+            data:record,
+            message:'successfully fetched records'
+        });
+        
+    });
+});
+
+router.put("/:client_id",[authJwt.verifyToken],  function(req,res){
+    const queryStr = 'select * from client_master where client_id=? and admin_user_name=?';
+    connection.query(queryStr, [req.params.client_id,req.username], async function (err, rows) {
+        let record = rows[0];
+        if(err || !record){
+            res.status(401).send({
+                error:true,
+                message:"Can't update client not found"
+            });
+            return;
+        }
+
+        ({profile_pic,client_name,country_id,state_id,city_id,location_id,block_id,firm_type,gst_number,representative_name,representative_id,phone_number,email_address,registration_details} = req.body);
+    if(!client_name || !country_id || !state_id || !city_id
+        || !location_id || !block_id || !gst_number || !representative_name
+        || !phone_number || !email_address || !registration_details || !firm_type){
+        res.status(401).send({
+                error:true,
+                message:"Please provide all fields !"
+            });
+        return;
+    }
+    if(!phone_number || phone_number.length != 10){
+        res.status(401).send({
+            error:true,
+            message:"Phone number not valid"
+        });
+        return;
+    }
+    if(!gst_number || gst_number.length != 15){
+        res.status(401).send({
+            error:true,
+            message:"GST number not valid"
+        });
+        return;
+    }
+   
+
+        const insertRecordToClientMaster = "update client_master set ? where client_id="+"'"+req.body.client_id+"'";
+        try{
+            let data = null;
+            let profile_id = null;
+            if(req.body.profile_pic){
+                data = await insertDocument(req.body.profile_pic,'profile_pic');
+                if(data){
+                    profile_id = data.insertId;
+                }
+                else{
+                    res.send({
+                        error:true,
+                        message:"Can't upload document"
+                    });
+                    return;
+                }
+            }
+
+            data = null;
+            let document_id = null;
+            if(req.body.document_file){
+               data = await insertDocument(req.body.document_file,'document_file');
+              
+               if(data){
+                   document_id = data.insertId;
+               }
+               else{
+                   res.send({
+                       error:true,
+                       message:"Can't upload document"
+                   });
+               }
+            }
+           
+           let dataToSave = {client_name,country_id,state_id,
+                city_id,location_id,block_id,
+                gst_number,representative_name,
+                representative_id,phone_number,
+                registration_details,
+                firm_type,
+            }
+            if( profile_id){
+                dataToSave.profile_id = profile_id;
+            }
+            if(document_id){
+                dataToSave.document_id = document_id;
+            }
+            connection.query(insertRecordToClientMaster, 
+            dataToSave, function(err, result) {
+                if(err){
+                   
+                    res.status(401).send({
+                        error:true,
+                        message:"Error updating client records."
+                    });
+                    return;
+                }
+                res.send({
+                    message:'Successfully updated Client Record'
+                });
+            });
+
+        }
+        catch(e){
+            console.log(e);
+            res.status(401).send({
+                error:true,
+                message:'Internal Server Error'
+            });
+            return;
+        }
+
+
+    });
+
+
+});
+
 router.post("/", [authJwt.verifyToken],  function(req,res){
   
     // getDocument(req,res);
@@ -10,21 +181,21 @@ router.post("/", [authJwt.verifyToken],  function(req,res){
     if(!client_name || !country_id || !state_id || !city_id
         || !location_id || !block_id || !gst_number || !representative_name
         || !phone_number || !email_address || !registration_details || !firm_type){
-        res.status(201).send({
+        res.status(401).send({
                 error:true,
                 message:"Please provide all fields !"
             });
         return;
     }
     if(!phone_number || phone_number.length != 10){
-        res.status(201).send({
+        res.status(401).send({
             error:true,
             message:"Phone number not valid"
         });
         return;
     }
     if(!gst_number || gst_number.length != 15){
-        res.status(201).send({
+        res.status(401).send({
             error:true,
             message:"GST number not valid"
         });
@@ -34,7 +205,7 @@ router.post("/", [authJwt.verifyToken],  function(req,res){
     var uniqueEmailAndPhoneNumber = 'select COUNT(*) AS count from client_master WHERE phone_number = ? or email_address = ?'
     connection.query(uniqueEmailAndPhoneNumber, [phone_number,email_address], function (err, rows) {
         if(err){
-            res.status(201).send({
+            res.status(401).send({
                 error:true,
                 message:"Something Went Wrong!"
             });
@@ -42,7 +213,7 @@ router.post("/", [authJwt.verifyToken],  function(req,res){
         }
         const count = rows[0].count;
         if(count) {
-            res.status(201).send({
+            res.status(401).send({
                 error:true,
                 message:"Email or phone Number is already Registered."
             });
@@ -70,9 +241,9 @@ router.post("/", [authJwt.verifyToken],  function(req,res){
             firstname:client_name,
             isActive:'Y'
         }, async function(err, result) {
-            console.log(err);
+         
             if(err){
-                res.status(201).send({
+                res.status(401).send({
                     error:true,
                     message:"Error inserting records."
                 });
@@ -81,29 +252,39 @@ router.post("/", [authJwt.verifyToken],  function(req,res){
 
             const insertRecordToClientMaster = "insert into client_master set ?";
             try{
-                let data =  await insertDocument(req.body.profile_pic,'profile_pic');
+                let data = null;
                 let profile_id = null;
-                if(data){
-                   file_id = data.insertId;
-                }
-                else{
-                    res.send({
-                        error:true,
-                        message:"Can't upload document"
-                    });
-                }
+                if(req.body.profile_pic){
+                    data = await insertDocument(req.body.profile_pic,'profile_pic');
+                    if(data){
+                        profile_id = data.insertId;
+                    }
+                    else{
+                        res.send({
+                            error:true,
+                            message:"Can't upload document"
+                        });
+                    }
+                }  
+               
+               
 
-                data =  await insertDocument(req.body.profile_pic,'document_file');
+                data = null;
                 let document_id = null;
-                if(data){
-                    document_id = data.insertId;
+                if(req.body.document_file){
+                   data = await insertDocument(req.body.document_file,'document_file');
+                   if(data){
+                        document_id = data.insertId;
+                    }
+                    else{
+                        res.send({
+                            error:true,
+                            message:"Can't upload document"
+                        });
+                    }
                 }
-                else{
-                    res.send({
-                        error:true,
-                        message:"Can't upload document"
-                    });
-                }
+                
+               
 
                 
                 connection.query(insertRecordToClientMaster, {client_name,country_id,state_id,
@@ -120,7 +301,7 @@ router.post("/", [authJwt.verifyToken],  function(req,res){
                     if(err){
                         connection.query('delete from login where username = ?',[loginUsrName],function(err,result){
     
-                            res.status(201).send({
+                            res.status(401).send({
                                 error:true,
                                 message:"Error inserting client records."
                             });
@@ -135,11 +316,10 @@ router.post("/", [authJwt.verifyToken],  function(req,res){
     
             }
             catch(e){
-                console.log(e);
                 
                 connection.query('delete from login where username = ?',[loginUsrName],function(err,result){
     
-                    res.status(201).send({
+                    res.status(401).send({
                         error:true,
                         message:e
                     });
@@ -157,10 +337,6 @@ router.post("/", [authJwt.verifyToken],  function(req,res){
     });
 });
 
-router.post("/profile_pic",function(req,res){
-    insertDocument(req.body.profile_pic,req,res);
-});
-
 
 
 
@@ -175,24 +351,25 @@ function bufferToBase64(buffer){
     return Buffer.from(buffer,'binary').toString('ascii');
 }
 
-async function getDocument(req,res){
-    const query = 'select * from base64_file where file_id = 3';
+async function getDocument(id){
+    const query = 'select * from base64_file where file_id = '+id;
     let record = null;
-    await connection.query(query,[],function(err,result){
-        console.log("error",err);
-        console.log("result", result);
-        let record = result[0]
-        if(err || !record){           
-           return null;
-        }
-        record = {file_name:result.file_name,file_type:result.file_data,file_id:result.file_id};
-        record.file_data = bufferToBase64(record.file_data);
+    record = await new Promise((resolve,reject)=>{
+        connection.query(query,[],function(err,results){
+            let result = results[0]
+            if(err || !result){           
+               return null;
+            }
+            record = {file_name:result.file_name,file_type:result.file_type,file_id:result.file_id};    
+            record.file_data = bufferToBase64(result.file_data);
+            resolve(record);
+        });
     });
     return record;
 }
 
 async function insertDocument(base64String,file_name){
-    console.log("filetye",file_name)
+   
     if(base64String){
         // console.log(base64String);
         const buffer = base64ToBuffr(base64String);
