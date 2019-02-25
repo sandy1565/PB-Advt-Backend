6b8b77e5f71c100b365712077c6ae735ae4d3b8c
sandy1565 <40385371+sandy1565@users.noreply.github.com>
2019-02-25 11:27:47 +0530

    cors

--- index.js
+++ index.js
@@ -14,60 +14,63 @@ const authJwt = require('./auth/verifyToken');
 var clientRouter  = require('./client-route');
 var cron = require('node-cron');
 let d = 0;
-cron.schedule('0 5 18 * * *', () => {
-  console.log('running every minute 1, 2, 4 and 5');
-  const allAdvrtQuery = `select * from advt_master`;
-  let today = new Date();
-  connection.query(allAdvrtQuery,function(err,results){
-    if(err || !results[0]){
-        console.log("1",err);
-        return;
-    }
-    // console.log("results",results)
-    let allAdverts = results;
-    const personsQuery =   `select * from person_master`;
-    connection.query(personsQuery,function(err,results){
-        if(err || !results[0]){
-            console.log("2",err);
-            return;
-        }
-        let allPersons = results;
-        // console.log(allPersons);
-        allAdverts.forEach(advert=>{
-            let publishDate = new Date(advert.publish_date);
-            let allEmails = [];
-            if(publishDate.getDate() == today.getDate() && 
-            publishDate.getMonth() == today.getMonth() && 
-            publishDate.getFullYear() == today.getFullYear()){
-                    let selectedPersons = allPersons.filter(person=>{
-                        
-                        return (
-                            advert.age_from <= person.age && person.age <= advert.age_to
-                        );
-                    });    
-                    selectedPersons.forEach(person=>{
-                        if(person.email_id){
-                            allEmails.push(person.email_id);
-                        }
-                    });
-                    if(allEmails.length){
-                        sendMessage(allEmails.join(","),advert.advt_subject,advert.advt_details,function(err){
-                            if(err){
-                               
-                                return;
-                            }
-                            console.log("SENT MESSAGES");
-                        });
-                    }
-                     
-            }
+cronJob('0 5 18 * * *');
+function cronJob(timePattern){
+    cron.schedule(timePattern, () => {
+        const allAdvrtQuery = `select * from advt_master`;
+        let today = new Date();
+        connection.query(allAdvrtQuery,function(err,results){
+          if(err || !results[0]){
+              console.log("1",err);
+              return;
+          }
+          // console.log("results",results)
+          let allAdverts = results;
+          const personsQuery =   `select * from person_master`;
+          connection.query(personsQuery,function(err,results){
+              if(err || !results[0]){
+                  console.log("2",err);
+                  return;
+              }
+              let allPersons = results;
+              // console.log(allPersons);
+              allAdverts.forEach(advert=>{
+                  let publishDate = new Date(advert.publish_date);
+                  let allEmails = [];
+                  if(publishDate.getDate() == today.getDate() && 
+                  publishDate.getMonth() == today.getMonth() && 
+                  publishDate.getFullYear() == today.getFullYear()){
+                          let selectedPersons = allPersons.filter(person=>{
+                              
+                              return (
+                                  advert.age_from <= person.age && person.age <= advert.age_to
+                              );
+                          });    
+                          selectedPersons.forEach(person=>{
+                              if(person.email_id){
+                                  allEmails.push(person.email_id);
+                              }
+                          });
+                          if(allEmails.length){
+                              sendMessage(allEmails.join(","),advert.advt_subject,advert.advt_details,function(err){
+                                  if(err){
+                                     
+                                      return;
+                                  }
+                                  console.log("SENT MESSAGES");
+                              });
+                          }
+                           
+                  }
+              });
+          });
         });
-    });
-  });
-},{
-    scheduled: true,
-    timezone: "Asia/Kolkata"
-  });
+      },{
+          scheduled: true,
+          timezone: "Asia/Kolkata"
+        });
+}
+
 connection.connect((err) => {
     if (err) {
         throw err;
