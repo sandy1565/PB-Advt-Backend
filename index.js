ab066a92a7207f17c45b82fe758bcdac7cda5336
sandy1565 <40385371+sandy1565@users.noreply.github.com>
2019-02-28 17:39:44 +0530

    c

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use('/public',express.static(path.resolve(__dirname, 'public')));
 app.use(cors());
 var urlencodedParser = bodyParser.urlencoded({ extended: false, parameterLimit: 100000, limit: '10mb' });
 app.use(bodyParser.json({ limit: '10mb' }));
-cronJob('*/3 * * * * *');
+cronJob('* */30 * * * *');
 
 
 function cronJob(timePattern) {
